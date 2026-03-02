cc9fd2a7546657530648461ffb75e100b3fa4c93
Aryan Agarwal <aryanagarwal12apr>
2026-03-02 21:56:36 +0530

    OOPS BANNER USING STRING

---
+++ OOPSBannerApp/UC2.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class oopsbannerapp1
+{
+public static void main(String[]args)
+{Scanner s=new Scanner(System.in);
+System.out.println(String.join(" ", "   ***   ", "   ***   ", " ******   ", "   ***** "));
+System.out.println(String.join(" ", " **   ** ", " **   ** ", " **    ** ", " **      "));
+System.out.println(String.join(" ", " **   ** ", " **   ** ", " **   **  ", "**       "));
+System.out.println(String.join(" ", " **   ** ", " **   ** ", " ******   ", "  *****  "));
+System.out.println(String.join(" ", " **   ** ", " **   ** ", " **       ", "      ** "));
+System.out.println(String.join(" ", " **   ** ", " **   ** ", " **       ", " **   ** "));
+System.out.println(String.join(" ", "   ***   ", "   ***   ", " **       ", "  *****  "));
+}
+}
