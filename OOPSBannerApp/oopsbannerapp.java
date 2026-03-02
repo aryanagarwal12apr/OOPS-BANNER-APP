6c38b48904e4568a077a8824bb45f09b909a7d1a
Aryan Agarwal <aryanagarwal12apr>
2026-03-02 19:52:31 +0530

    PRINTING OOPS

--- OOPSBannerApp/oopsbannerapp.java
+++ OOPSBannerApp/oopsbannerapp.java
@@ -3,12 +3,6 @@ class oopsbannerapp
 {
 public static void main(String[]args)
 {Scanner s=new Scanner(System.in);
-System.out.println("   ***   " + " " + "   ***** ");
-System.out.println(" **   ** " + " " + " **      ");
-System.out.println(" **   ** " + " " + "**       ");
-System.out.println(" **   ** " + " " + "  *****  ");
-System.out.println(" **   ** " + " " + "      ** ");
-System.out.println(" **   ** " + " " + " **   ** ");
-System.out.println("   ***   " + " " + "  *****  ");
+System.out.println("OOPS");
 }
 }
